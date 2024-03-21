782b320341a85381c5a212b4c6d22ef939cdbb20
michqo <42279279+michqo@users.noreply.github.com>
2024-03-21 12:24:56 +0100

    feat: add fiber logger

--- cmd/main.go
+++ cmd/main.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func main() {
@@ -16,6 +17,7 @@ func main() {
 
 	app := fiber.New()
 	app.Use(cors.New())
+	app.Use(logger.New())
 
 	api := app.Group("/v1")
 	routes.CreateRoutes(api)
